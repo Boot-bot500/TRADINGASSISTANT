69c15203fcc18ce53ff3ae81cf2ebbdbc9e3563d
Boot-bot500 <morell3@gmail.com>
2025-05-18 22:47:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -27,3 +27,8 @@ def message():
     @app.route('/')
 def index():
     return '¡Trading Assistant está corriendo!'
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
