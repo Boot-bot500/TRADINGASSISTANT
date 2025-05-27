51396d06b1b00947137ecda4a7552cf7e555dbfe
Boot-bot500 <morell3@gmail.com>
2025-05-26 20:24:13 -0400

    Update quiz_service.py

--- quiz_service.py
+++ quiz_service.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
 def cargar_preguntas():
-    df = pd.read_excel("preguntas.xlsx")
-    return df.to_dict(orient='records')
+    df = pd.read_excel("analisis_trading.xlsx")
+    preguntas = df["Pregunta"].tolist()
+    return preguntas
